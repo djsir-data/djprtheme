ac7e38dcce2ea1a8a464517406c330d36650912f
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2021-04-22 11:04:03 +1000

    First commit, empty function

---
+++ R/djpr_save_image.R
@@ -0,0 +1,9 @@
+#' A function that is a wrapper around ggplot2::ggsave().
+#' This enables users to save images in certain pre-defined sizes: full slide, half slide,
+#' 2/3rds slide, and 1/4 slide. These sizes will be appropriate for inclusion in a DJPR PPT deck.
+#' The function should remove title and subtitle by default, as these
+#' should be added on the slide itself rather than included in the image.
+
+djpr_save_image <- function(filename, objectname, size) {
+
+}
