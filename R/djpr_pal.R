e4bc78121fd4e5d0c4e2eee4fd4484cb9ce708dc
Darren J Wong (DJPR) <darren.wong@ecodev.vic.gov.au>
2021-04-26 10:56:22 +1000

    Initial Colour Palette

---
+++ R/djpr_pal.R
@@ -0,0 +1,154 @@
+#' Create a Djpr-appropriate palette for your chart.
+#'
+#' @param n Numeric. The number of levels in your colour scale. Minimum value is
+#'   1, maximum is 10. Using more than 6 is not recommended. If you don't
+#'   specify `n`, a five-colour palette will be used, which may not look right.
+#'   Specify `n`.
+#'
+#'   By default, n = 2 will give you light orange and dark orange. Use n = "2a"
+#'   if you want light orange and cool_grey_11.
+#' @param reverse Logical. FALSE by default. Setting to TRUE reverses the
+#'   standard colour order. Standard colour order runs from light to dark. If
+#'   you set reverse to TRUE, colours will run from dark to light.
+#' @param faded Logical. FALSE by default. Setting to TRUE returns the faded
+#'   variations of the standard colours.
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' p <- ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+#'     geom_point() +
+#'     theme_djpr() +
+#'     scale_colour_manual(values = djpr_pal(n = 3))
+#'
+#' p
+#'
+#' # Alternatively, use djpr_colour_manual(), which is a wrapper
+#' # around scale_colour_manual():
+#'
+#' p <- ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+#'     geom_point() +
+#'     theme_djpr() +
+#'     djpr_colour_manual(n = 3)
+#'
+#' p
+#'
+#' @export
+
+
+
+
+djpr_pal <- function(n = 0, reverse = FALSE, faded = FALSE) {
+
+
+  if (n == 0) {
+    n <- 5
+    "Your chart will probably look better if you specify n in djpr_pal()."
+  }
+
+
+  if (n > 6 & n <= 10) {
+    warning("Using more than six colours is not recommended.")
+  }
+
+
+  if (n > 10 & n != "2a") {
+    stop(paste0("You've requested ", n,
+                " colours; djpr_pal() only supports up to 10."))
+  }
+
+
+  if (isFALSE(faded)) {
+    palette <- regular_palette(n)
+  }
+
+
+  if (isTRUE(faded)) {
+    palette <- faded_palette(n)
+  }
+
+
+  if (isTRUE(reverse)) {
+    palette <- rev(palette)
+  }
+
+
+  palette
+}
+
+
+regular_palette <- function(n) {
+
+
+  if (n == 1) {
+    palette <- djprtheme::djpr_blue
+  } else if (n == "2a") {
+    palette <- c(djprtheme::djpr_pacific_blue,
+                 djprtheme::djpr_blue)
+  } else if (n == 2) {
+    palette <- c(djprtheme::djpr_blue,
+                 djprtheme::djpr_green)
+  } else if (n == 3) {
+    palette <- c(djprtheme::djpr_pacific_blue,
+                 djprtheme::djpr_cool_grey_1,
+                 djprtheme::djpr_blue)
+  } else if (n == 4) {
+    palette <- c(djprtheme::djpr_pacific_blue,
+                 djprtheme::djpr_blue,
+                 djprtheme::djpr_navy_blue,
+                 djprtheme::djpr_cool_grey_11)
+  } else if (n == 5) {
+    palette <- c(djprtheme::djpr_pacific_blue,
+                 djprtheme::djpr_blue,
+                 djprtheme::djpr_navy_blue,
+                 djprtheme::djpr_cool_grey_11,
+                 djprtheme::djpr_cool_grey_1)
+  } else if (n == 6) {
+    palette <- c(djprtheme::djpr_iris_blue,
+                 djprtheme::djpr_pacific_blue,
+                 djprtheme::djpr_blue,
+                 djprtheme::djpr_navy_blue,
+                 djprtheme::djpr_cool_grey_11,
+                 djprtheme::djpr_cool_grey_1)
+  } else if (n == 7) {
+    palette <- c(djprtheme::djpr_iris_blue,
+                 djprtheme::djpr_pacific_blue,
+                 djprtheme::djpr_blue,
+                 djprtheme::djpr_navy_blue,
+                 djprtheme::djpr_cool_grey_11,
+                 djprtheme::djpr_cool_grey_1,
+                 "black")
+  } else if (n == 8) {
+    palette <- c(djprtheme::djpr_iris_blue,
+                 djprtheme::djpr_pacific_blue,
+                 djprtheme::djpr_blue,
+                 djprtheme::djpr_navy_blue,
+                 djprtheme::djpr_cool_grey_11,
+                 djprtheme::djpr_cool_grey_1, "black",
+                 djprtheme::djpr_grey4)
+  } else if (n == 9) {
+    palette <- c(djprtheme::djpr_iris_blue,
+                 djprtheme::djpr_pacific_blue,
+                 djprtheme::djpr_blue,
+                 djprtheme::djpr_navy_blue,
+                 djprtheme::djpr_cool_grey_11,
+                 djprtheme::djpr_cool_grey_1,
+                 "black",
+                 djprtheme::djpr_grey4,
+                 djprtheme::djpr_grey3)
+  } else if (n == 10) {
+    palette <- c(djprtheme::djpr_iris_blue,
+                 djprtheme::djpr_pacific_blue,
+                 djprtheme::djpr_blue,
+                 djprtheme::djpr_navy_blue,
+                 djprtheme::djpr_cool_grey_11,
+                 djprtheme::djpr_cool_grey_1,
+                 "black",
+                 djprtheme::djpr_grey4,
+                 djprtheme::djpr_grey3,
+                 djprtheme::djpr_grey2)
+  }
+  palette
+}
+
+
