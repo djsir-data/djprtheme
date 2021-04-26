a592fcb753c9ff0dbcb1a305a9b7d03a57d18f4e
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2021-04-27 09:45:00 +1000

    First go at defining sizes. Just guessing dimensions for now.

--- R/djpr_save_image.R
+++ R/djpr_save_image.R
@@ -4,6 +4,34 @@
 #' The function should remove title and subtitle by default, as these
 #' should be added on the slide itself rather than included in the image.
 
-djpr_save_image <- function(filename, objectname, size = ) {
+# "retina" = 320 dpi
+djpr_save_image <- function(filename, plot = last_plot(), size = "full", dpi = "retina") {
+
+# remove title
+
+# remove subtitle
+
+# define sizes
+if (size == "full") {
+  width == "20"
+  height == "20"}
+
+if (size == "threequarter") {
+    width == "15"
+    height == "15"}
+
+if (size == "half") {
+    width == "10"
+    height == "10"}
+
+if (size == "quarter") {
+    width == "5"
+    height == "5"}
+
+else {print("Error: Parameter size not properly defined. Options are 'full, 'threequarter', 'half' and 'quarter'")}
+
+# ggsave function
+ggsave(filename=filename, plot = plot, width = width, height = height, units = "cm", dpi=dpi)
 
 }
+
