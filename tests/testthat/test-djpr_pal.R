94dc5ad46d5d5d72af910e9e1b50c41bb1bb4fe2
Matt Cowgill <mattcowgill@gmail.com>
2021-04-29 08:38:13 +1000

    add visual unit test for colour pyramid

--- tests/testthat/test-djpr_pal.R
+++ tests/testthat/test-djpr_pal.R
@@ -15,3 +15,44 @@ test_that("djpr_*_manual() wrappers work", {
   expect_s3_class(djpr_colour_manual(5), "ggproto")
   expect_s3_class(djpr_fill_manual(5), "ggproto")
 })
+
+
+# Colour pyramid ----
+# The pyramid shows the default colours and their ordering
+library(dplyr, warn.conflicts = F)
+library(ggplot2, warn.conflicts = F)
+make_col_tibble <- function(n) {
+  dplyr::tibble(col = suppressWarnings(djpr_pal(n)),
+                order = c(1:n),
+                n = n)
+}
+
+djpr_colours <- lapply(c(1:10), make_col_tibble) %>%
+  dplyr::bind_rows()
+
+djpr_colours$col <- factor(djpr_colours$col)
+
+djpr_colours <- djpr_colours %>%
+  dplyr::mutate(n_desc = dplyr::if_else(n == 1, "n = 1", paste0("  ", n)))
+
+pyramid <- djpr_colours %>%
+  ggplot(aes(x = reorder(n_desc, -n),
+             y = order,
+             fill = col)) +
+  geom_tile(col = "white", size = 1) +
+  scale_fill_manual(values = levels(djpr_colours$col)) +
+  coord_flip(expand = FALSE, ylim = c(0, 13)) +
+  theme_djpr() +
+  theme(panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        axis.line = element_blank(),
+        axis.text.x = element_blank(),
+        legend.position = "none") +
+  labs(title = "These are the colours of the DJPR palette")
+
+test_that("Colour pyramid is unchanged", {
+  vdiffr::expect_doppelganger("pyramid",
+                              pyramid,
+                              path = "")
+})
