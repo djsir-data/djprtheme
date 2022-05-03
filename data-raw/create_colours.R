2f6d2fe9e6f97d3b69b9d144631f4733cf1bde70
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2022-05-03 16:00:15 +1000

    slight change to colour 'watercourse' to improve accessibility.

--- data-raw/create_colours.R
+++ data-raw/create_colours.R
@@ -4,13 +4,12 @@ djpr_blue <- "#1F1547"
 djpr_bondi_blue <- "#1D9EC3"
 djpr_royal_blue <- "#2A6FA2"
 djpr_green <- "#62BB46"
-djpr_watercourse <- "#00573f"   #replaced for accessibility
+djpr_watercourse <- "#00573f"   #replaced to improve accessibility
 djpr_cool_grey_11 <- "#53565A"
 djpr_cobalt <- "#004676"
 djpr_faint_blue <- "#BCD3EF"
 djpr_purple <- "#745ECF"
 djpr_faint_green <- "#C0E4B5"
-
 
 usethis::use_data(djpr_blue,
   djpr_bondi_blue,
